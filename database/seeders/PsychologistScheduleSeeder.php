4871591bcd21c46a428ac207f9a8d92b47b2aabc
Bryan Cornelius S <145685324+KUCINGOREN8@users.noreply.github.com>
2025-12-02 16:14:11 +0700

    Refactor psychologist_id assignment in seeder

--- database/seeders/PsychologistScheduleSeeder.php
+++ database/seeders/PsychologistScheduleSeeder.php
@@ -14,9 +14,6 @@ class PsychologistScheduleSeeder extends Seeder
      */
     public function run(): void
     {
-        $schedules = [
-            [
-                'psychologist_id' => '1',
         $psychologist = Psychologist::whereHas('user', function($q) {
             $q->where('email', 'doctor@gmail.com');
         })->first();
@@ -29,31 +26,19 @@ class PsychologistScheduleSeeder extends Seeder
                 'end_time' => '15:00',
             ],
             [
-<<<<<<< HEAD
-                'psychologist_id' => '1',
-=======
                 'psychologist_id' => $psychologist->id,
->>>>>>> 05965638d654be5556a9a63ac9d22ecc8010904b
                 'day_of_week' => 'tue',
                 'start_time' => '10:00',
                 'end_time' => '14:00',
             ],
             [
-<<<<<<< HEAD
-                'psychologist_id' => '1',
-=======
                 'psychologist_id' => $psychologist->id,
->>>>>>> 05965638d654be5556a9a63ac9d22ecc8010904b
                 'day_of_week' => 'thu',
                 'start_time' => '09:00',
                 'end_time' => '17:00',
             ],
             [
-<<<<<<< HEAD
-                'psychologist_id' => '1',
-=======
                 'psychologist_id' => $psychologist->id,
->>>>>>> 05965638d654be5556a9a63ac9d22ecc8010904b
                 'day_of_week' => 'sat',
                 'start_time' => '10:00',
                 'end_time' => '13:00',
