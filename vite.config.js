aa7f9edee04e98871f8783ce44523e3400c75d1e
Bryan Cornelius Sunadi <bryansunadi@gmail.com>
2025-12-05 10:43:13 +0700

    Remove laravel-vite-plugin, use plain Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel({
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        rollupOptions: {
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
+        },
+    },
 });
