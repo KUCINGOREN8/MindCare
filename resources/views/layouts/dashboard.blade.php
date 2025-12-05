e496dee84a3a555427b1621daf450cbd1b7b85f1
Bryan Cornelius Sunadi <bryansunadi@gmail.com>
2025-12-05 12:25:17 +0700

    Emergency: Switch to CDN for Tailwind

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,8 +8,8 @@
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.14/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js" defer></script>
-    <link href="{{ asset('build/output.css') }}" rel="stylesheet">
-    @stack('styles')
+    {{-- <link href="{{ asset('build/output.css') }}" rel="stylesheet"> --}}
+    {{-- @stack('styles') --}}
 </head>
 <body class="bg-background font-sans min-h-screen flex">
     @include('components.navbar')
@@ -21,6 +21,6 @@
     </main>
 
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    @stack('scripts')
+    {{-- @stack('scripts') --}}
 </body>
 </html>
