c67d66e3e6f8542b01afd7605e85edaa526adcc9
Bryan Cornelius Sunadi <bryansunadi@gmail.com>
2025-12-15 03:02:18 +0700

    feat: fixed Appointment Model

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -125,10 +125,12 @@ class Appointment extends Model
                     $q2->whereDate('date', '=', now()->format('Y-m-d'))->whereTime('end_time', '<=', now()->format('H:i:s'));
                 });
         });
+    }
+
     public function getCanBeReviewedAttribute()
     {
-        return $this->status === 'completed' && 
-            auth()->check() && 
+        return $this->status === 'completed' &&
+            auth()->check() &&
             auth()->id() === $this->user_id;
     }
 
@@ -137,7 +139,7 @@ class Appointment extends Model
         if (!$this->can_be_reviewed) {
             return false;
         }
-        
+
         return $this->psychologist->reviews()
             ->where('user_id', auth()->id())
             ->exists();
