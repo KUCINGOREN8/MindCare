a95ea9dfe2ec588f6400022f4fdc854583073a7a
charellegw <147069930+charellegw@users.noreply.github.com>
2025-12-14 21:24:50 +0700

    Fix: appointment controller

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -232,7 +232,7 @@ class AppointmentController extends Controller
             ->orderBy('created_at', 'desc')
             ->get();
 
-        return view('psychologist.appointments.index', compact(
+        return view('psychologist.appointment.index', compact(
             'upcomingAppointments',
             'todayAppointments',
             'pendingAppointments'
