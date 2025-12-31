af84ddaef2cae279e428b10d17794cdbfbac10d2
Bryan Cornelius Sunadi <bryansunadi@gmail.com>
2025-12-31 13:21:36 +0700

    feat: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
+        "./app/View/**/*.php",
         "./resources/views/**/*.blade.php",
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
+        "./resources/views/components/**/*.blade.php",
+        "./resources/js/**/*.js",
         "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     ],
 
     theme: {
